db4b6d32417d09ef9c85494ea1bad3bb2ccb454b
Gunika505 <162486134+Gunika505@users.noreply.github.com>
2024-03-07 05:12:23 +0530

    Update assembler1.py

--- assembler1.py
+++ assembler1.py
@@ -1,2 +1,26 @@
-print("helloworld")
-
+def readassembly():
+  return 0
+def binconv():
+  return 0
+#gunika
+regdict={}
+instrtypes={"R":[],"I":[],"J":[],"S":[],"B":[],"U":[]}
+def identifyinstrtype():
+  #ifelse for each
+  return 0
+#khushi
+def identifyRinstr(l):
+  return 0
+def identifyIinstr(lst):
+  return 0
+def identifyJinstr(lst):
+  return 0
+#kanu
+def identifyBinstr(lst):
+  return 0
+def identifyUinstr(lst):
+  return 0
+def identifySinstr(lst):
+  return 0
+def converttobinary():
+  return 0
