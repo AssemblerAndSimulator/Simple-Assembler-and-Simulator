2777ffa493da3c5622ec1908b9d96dacb515726a
Kanupriyag <kanupriya23269@iiitd.ac.in>
2024-03-11 20:24:26 +0530

    update

--- assembler1.py
+++ assembler1.py
@@ -19,6 +19,7 @@ regdict={'x0':'00000','x1':'00001','x2':'00010','x3':'00011','x4':'00100','x5':'
 abidict={'zero':'00000','ra':'00001','sp':'00010','gp':'00011','tp':'00100','t0':'00101','t1':'00110','t2':'00111','s0':'01000','fp':'01000','s1':'01001','a0':'01010','a1':'01011','a2':'01100','a3':'01101','a4':'01110','a5':'01111','a6':'10000','a7':'10001','s2':'10010','s3':'10011','s4':'10100','s5':'10101','s6':'10110','s7':'10111','s8':'11000','s9':'11001','s10':'11010','s11':'11011','t3':'11100','t4':'11101','t5':'11110','t6':'11111'}
 #gunika
 instrtypes={"R":['add','sub','slt','sltu','and','or','xor','sll','srl'],"I":["lw","addi","sltiu","jalr"],"J":["jal"],"S":["sw"],"B":["beq","bne","blt","bge","bltu","bgeu"],"U":["lui","auipc"]}
+allinstr=['add','sub','slt','sltu','and','or','xor','sll','srl',"lw","addi","sltiu","jalr","jal","sw","beq","bne","blt","bge","bltu","bgeu","lui","auipc"]
 def identifyinstrtype(str1):
   for i in instrtypes:
     if str1 in instrtypes[i]:
@@ -141,6 +142,8 @@ mainlst=readassembly()
 for i in mainlst:
   newi=i.replace(","," ").replace("("," ").replace(")"," ")
   a=newi.split()
+  if a[0] not in allinstr:
+    a=a[1:]
   type=identifyinstrtype(a[0])
   if type=='I':
     print(identifyIinstr(a))
